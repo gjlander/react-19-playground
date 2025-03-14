5c24615f57cd13732b7e5941c6b5d7134b6e99cd
Garrett Lander <garrett.jlander@gmail.com>
2025-03-14 18:17:39 +0100

    using deployed backend

--- src/data/ducks.js
+++ src/data/ducks.js
@@ -1,5 +1,5 @@
-// const BASE_URL = 'https://duckpond-89zn.onrender.com/wild-ducks';
-const BASE_URL = 'http://localhost:3000/wild-ducks';
+const BASE_URL = 'https://duckpond-89zn.onrender.com/wild-ducks';
+// const BASE_URL = 'http://localhost:3000/wild-ducks';
 
 const getAllDucks = async () => {
     const res = await fetch(BASE_URL);
